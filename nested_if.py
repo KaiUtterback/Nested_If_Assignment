8fb2cf07fa98015850f6492e82dcc6c9df70d84c
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-02-09 21:34:31 -0600

    Working out kinks in task 1

--- nested_if.py
+++ nested_if.py
@@ -5,20 +5,23 @@ print("")
 place = input("Choose a place: forest or cave? ")
 
 if place == "forest":
-    action = input ("climb a tree or cross a river")
+    action = input ("climb a tree or cross a river? ")
     if action == "climb a tree":
         print("You found a bird's nest!")
-    elif action == "cross a river":
-        print("You found a boat!")    
+    elif action != "climb a tree":
+            print("You found a boat!")  
 elif place == "cave":
     print("You find a hidden treasure")
-    
+else: 
+    pass
+
 # Task 2: Expand the game. If the user chooses "cave", ask them if they want to light a torch
 # or proceed in the dark.
-print(" ")
 
 if place == "cave":
     cave_option_1 = input("do you want to light a torch, or proceed in the dark? ")
     
 # Task 3: Default Path
+
+
     
