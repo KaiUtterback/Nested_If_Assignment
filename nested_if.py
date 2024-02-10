3955eb9c7912903c02d10cd84e426e925f773bd0
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-02-09 22:14:30 -0600

    lost

--- nested_if.py
+++ nested_if.py
@@ -30,6 +30,18 @@ if place == "cave":
 
 attendees = int(input("Enter number of attendees: "))
 if attendees > 100: venue = "large hall"
-if attendees < 100: venue = "conference room"
+elif attendees < 100: venue = "conference room"
+print(venue)
 
-# venue = "large hall" if attendees > 100 else "conference room" = venue 
+# Task 2: Venue Selection: Enhance the program to reccomend additional facilities like 
+# audio system or projector based on number of attendees.
+
+if attendees >= 15: audio_system = True
+if attendees > 40: projector = True
+
+if audio_system == True and projector == False:
+    print("We think you should add an audio system to your event package")
+elif projector == True and audio_system == False:
+    print("We think you should add a projector to your event package")
+elif audio_system and projector == True: 
+    print("We think you should add an audio system and projector to your event package")
